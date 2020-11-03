433c99ff66f5d3a9bda41a3cb21e110ac4bafc5b
ziltoid1991 <laszloszeremi@outlook.com>
2020-11-04 00:40:49 +0100

    End of refactoring

---
+++ src/CPUblit/composing/specblt.d
@@ -0,0 +1,502 @@
+module CPUblit.composing.specblt;
+
+import CPUblit.composing.common;
+
+/**
+ * Text blitter, mainly intended for single color texts, can work in other applications as long as they're correctly formatted,
+ * meaning: transparent pixels = 0, colored pixels = T.max 
+ */
+public void textBlitter(T)(T* src, T* dest, size_t length, T color) @nogc pure nothrow {
+	static if(USE_INTEL_INTRINSICS){
+		static if(T.stringof == "ubyte"){
+			__vector(ubyte[16]) colorV;
+			static enum MAINLOOP_LENGTH = 16;
+			static enum HALFLOAD_LENGTH = 8;
+			static enum QUTRLOAD_LENGTH = 4;
+		}else static if(T.stringof == "ushort"){
+			__vector(ushort[8]) colorV;
+			static enum MAINLOOP_LENGTH = 8;
+			static enum HALFLOAD_LENGTH = 4;
+			static enum QUTRLOAD_LENGTH = 2;
+		}else static if(T.stringof == "uint"){
+			__vector(uint[4]) colorV;
+			static enum MAINLOOP_LENGTH = 4;
+			static enum HALFLOAD_LENGTH = 2;
+			static enum QUTRLOAD_LENGTH = 1;
+		}else static assert(0, "Template parameter '"~ T.stringof ~"' not supported!");
+		static foreach(i; 0 .. (MAINLOOP_LENGTH)){
+			colorV[i] = color;
+		}
+		while(length >= MAINLOOP_LENGTH){
+			__m128i srcV = _mm_loadu_si128(cast(__m128i*)src) & cast(__m128i)colorV;
+			__m128i destV = _mm_loadu_si128(cast(__m128i*)dest);
+			static if(T.stringof == "ubyte")
+				__m128i mask = _mm_cmpeq_epi8(srcV, SSE2_NULLVECT);
+			else static if(T.stringof == "ushort")
+				__m128i mask = _mm_cmpeq_epi16(srcV, SSE2_NULLVECT);
+			else static if(T.stringof == "uint")
+				__m128i mask = _mm_cmpeq_epi32(srcV, SSE2_NULLVECT);
+			destV = srcV | (destV & mask);
+			_mm_storeu_si128(cast(__m128i*)dest, destV);
+			src += MAINLOOP_LENGTH;
+			dest += MAINLOOP_LENGTH;
+			length -= MAINLOOP_LENGTH;
+		}
+		if(length >= HALFLOAD_LENGTH){
+			__m128i srcV = _mm_loadl_epi64(cast(__m128i*)src) & cast(__m128i)colorV;
+			__m128i destV = _mm_loadl_epi64(cast(__m128i*)dest);
+			static if(T.stringof == "ubyte")
+				__m128i mask = _mm_cmpeq_epi8(srcV, SSE2_NULLVECT);
+			else static if(T.stringof == "ushort")
+				__m128i mask = _mm_cmpeq_epi16(srcV, SSE2_NULLVECT);
+			else static if(T.stringof == "uint")
+				__m128i mask = _mm_cmpeq_epi32(srcV, SSE2_NULLVECT);
+			destV = srcV | (destV & mask);
+			_mm_storel_epi64(cast(__m128i*)dest, destV);
+			src += HALFLOAD_LENGTH;
+			dest += HALFLOAD_LENGTH;
+			length -= HALFLOAD_LENGTH;
+		}
+		if(length >= QUTRLOAD_LENGTH){
+			__m128i srcV = _mm_cvtsi32_si128((*cast(int*)src)) & cast(__m128i)colorV;
+			__m128i destV = _mm_cvtsi32_si128((*cast(int*)dest));
+			static if(T.stringof == "ubyte")
+				__m128i mask = _mm_cmpeq_epi8(srcV, SSE2_NULLVECT);
+			else static if(T.stringof == "ushort")
+				__m128i mask = _mm_cmpeq_epi16(srcV, SSE2_NULLVECT);
+			else static if(T.stringof == "uint")
+				__m128i mask = _mm_cmpeq_epi32(srcV, SSE2_NULLVECT);
+			destV = srcV | (destV & mask);
+			*cast(int*)dest = _mm_cvtsi128_si32(destV);
+			static if(T.stringof != "uint"){
+				src += QUTRLOAD_LENGTH;
+				dest += QUTRLOAD_LENGTH;
+				length -= QUTRLOAD_LENGTH;
+			}
+		}
+		static if(T.stringof == "ubyte"){
+			while(length){
+				const ubyte mask = *src ? ubyte.min : ubyte.max;
+				*dest = (*src & color) | (*dest & mask);
+				src++;
+				dest++;
+				length--;
+			}
+		}else static if(T.stringof == "ushort"){
+			if(length){
+				const ushort mask = *src ? ushort.min : ushort.max;
+				*dest = (*src /+& color+/) | (*dest & mask);
+			}
+		}
+	}else{
+		while(length){
+			const ubyte mask = *src ? T.min : T.max;
+			*dest = (*src & color) | (*dest & mask);
+			src++;
+			dest++;
+			length--;
+		}
+	}
+}
+/**
+ * Text blitter, mainly intended for single color texts, can work in other applications as long as they're correctly formatted,
+ * meaning: transparent pixels = 0, colored pixels = T.max 
+ */
+public void textBlitter(T)(T* src, T* dest, T* dest0, size_t length, T color) @nogc pure nothrow {
+	static if(USE_INTEL_INTRINSICS){
+		static if(T.stringof == "ubyte"){
+			__vector(ubyte[16]) colorV;
+			static enum MAINLOOP_LENGTH = 16;
+			static enum HALFLOAD_LENGTH = 8;
+			static enum QUTRLOAD_LENGTH = 4;
+		}else static if(T.stringof == "ushort"){
+			__vector(ushort[8]) colorV;
+			static enum MAINLOOP_LENGTH = 8;
+			static enum HALFLOAD_LENGTH = 4;
+			static enum QUTRLOAD_LENGTH = 2;
+		}else static if(T.stringof == "uint"){
+			__vector(uint[4]) colorV;
+			static enum MAINLOOP_LENGTH = 4;
+			static enum HALFLOAD_LENGTH = 2;
+			static enum QUTRLOAD_LENGTH = 1;
+		}else static assert(0, "Template parameter '"~ T.stringof ~"' not supported!");
+		static foreach(i; 0 .. (MAINLOOP_LENGTH)){
+			colorV[i] = color;
+		}
+		while(length >= MAINLOOP_LENGTH){
+			__m128i srcV = _mm_loadu_si128(cast(__m128i*)src) & cast(__m128i)colorV;
+			__m128i destV = _mm_loadu_si128(cast(__m128i*)dest);
+			static if(T.stringof == "ubyte")
+				__m128i mask = _mm_cmpeq_epi8(srcV, SSE2_NULLVECT);
+			else static if(T.stringof == "ushort")
+				__m128i mask = _mm_cmpeq_epi16(srcV, SSE2_NULLVECT);
+			else static if(T.stringof == "uint")
+				__m128i mask = _mm_cmpeq_epi32(srcV, SSE2_NULLVECT);
+			destV = srcV | (destV & mask);
+			_mm_storeu_si128(cast(__m128i*)dest0, destV);
+			src += MAINLOOP_LENGTH;
+			dest += MAINLOOP_LENGTH;
+			dest0 += MAINLOOP_LENGTH;
+			length -= MAINLOOP_LENGTH;
+		}
+		if(length >= HALFLOAD_LENGTH){
+			__m128i srcV = _mm_loadl_epi64(cast(__m128i*)src) & cast(__m128i)colorV;
+			__m128i destV = _mm_loadl_epi64(cast(__m128i*)dest);
+			static if(T.stringof == "ubyte")
+				__m128i mask = _mm_cmpeq_epi8(srcV, SSE2_NULLVECT);
+			else static if(T.stringof == "ushort")
+				__m128i mask = _mm_cmpeq_epi16(srcV, SSE2_NULLVECT);
+			else static if(T.stringof == "uint")
+				__m128i mask = _mm_cmpeq_epi32(srcV, SSE2_NULLVECT);
+			destV = srcV | (destV & mask);
+			_mm_storel_epi64(cast(__m128i*)dest0, destV);
+			src += HALFLOAD_LENGTH;
+			dest += HALFLOAD_LENGTH;
+			dest0 += HALFLOAD_LENGTH;
+			length -= HALFLOAD_LENGTH;
+		}
+		if(length >= QUTRLOAD_LENGTH){
+			__m128i srcV = _mm_cvtsi32_si128((*cast(int*)src)) & cast(__m128i)colorV;
+			__m128i destV = _mm_cvtsi32_si128((*cast(int*)dest));
+			static if(T.stringof == "ubyte")
+				__m128i mask = _mm_cmpeq_epi8(srcV, SSE2_NULLVECT);
+			else static if(T.stringof == "ushort")
+				__m128i mask = _mm_cmpeq_epi16(srcV, SSE2_NULLVECT);
+			else static if(T.stringof == "uint")
+				__m128i mask = _mm_cmpeq_epi32(srcV, SSE2_NULLVECT);
+			destV = srcV | (destV & mask);
+			*cast(int*)dest0 = _mm_cvtsi128_si32(destV);
+			static if(T.stringof != "uint"){
+				src += QUTRLOAD_LENGTH;
+				dest += QUTRLOAD_LENGTH;
+				dest0 += QUTRLOAD_LENGTH;
+				length -= QUTRLOAD_LENGTH;
+			}
+		}
+		static if(T.stringof == "ubyte"){
+			while(length){
+				const T mask = *src ? T.min : T.max;
+				*dest0 = (*src & color) | (*dest & mask);
+				src++;
+				dest++;
+				dest0++;
+				length--;
+			}
+		}else static if(T.stringof == "ushort"){
+			if(length){
+				const T mask = *src ? T.min : T.max;
+				*dest0 = (*src & color) | (*dest & mask);
+			}
+		}
+	}else{
+		while(length){
+			const T mask = *src ? T.min : T.max;
+			*dest0 = (*src & color) | (*dest & mask);
+			src++;
+			dest++;
+			dest0++;
+			length--;
+		}
+	}
+}
+/**
+ * XOR blitter. Popularly used for selection and pseudo-transparency.
+ */
+public @nogc void xorBlitter(T)(T* dest, T* dest0, size_t length, T color){
+	static if(USE_INTEL_INTRINSICS){
+		static if(T.stringof == "ubyte"){
+			__vector(ubyte[16]) colorV;
+			static enum MAINLOOP_LENGTH = 16;
+			static enum HALFLOAD_LENGTH = 8;
+			static enum QUTRLOAD_LENGTH = 4;
+		}else static if(T.stringof == "ushort"){
+			__vector(ushort[8]) colorV;
+			static enum MAINLOOP_LENGTH = 8;
+			static enum HALFLOAD_LENGTH = 4;
+			static enum QUTRLOAD_LENGTH = 2;
+		}else static if(T.stringof == "uint"){
+			__vector(uint[4]) colorV;
+			static enum MAINLOOP_LENGTH = 4;
+			static enum HALFLOAD_LENGTH = 2;
+			static enum QUTRLOAD_LENGTH = 1;
+		}else static assert(0, "Template parameter '"~ T.stringof ~"' not supported!");
+		for (int i ; i < colorV.length ; i++){
+			colorV[i] = color;
+		}
+		while(length >= MAINLOOP_LENGTH){
+			__m128i destV = _mm_loadu_si128(cast(__m128i*)dest);
+			destV = _mm_xor_si128(destV, colorV);
+			_mm_storeu_si128(cast(__m128i*)dest0, destV);
+			dest += MAINLOOP_LENGTH;
+			dest0 += MAINLOOP_LENGTH;
+			length -= MAINLOOP_LENGTH;
+		}
+		if(length >= HALFLOAD_LENGTH){
+			__m128i destV = _mm_loadl_epi64(cast(__m64*)dest);
+			destV = _mm_xor_si128(destV, colorV);
+			_mm_storel_epi64(cast(__m64*)dest0, destV);
+			dest += HALFLOAD_LENGTH;
+			dest0 += HALFLOAD_LENGTH;
+			length -= HALFLOAD_LENGTH;
+		}
+		if(length >= QUTRLOAD_LENGTH){
+			__m128i destV = _mm_cvtsi32_si128((*cast(int*)dest));
+			destV = _mm_xor_si128(destV, colorV);
+			*cast(int*)dest0 = _mm_cvtsi128_si32(destV);
+			static if(T.stringof != "uint"){
+				dest += QUTRLOAD_LENGTH;
+				dest0 += QUTRLOAD_LENGTH;
+				length -= QUTRLOAD_LENGTH;
+			}
+		}
+		static if(T.stringof == "ubyte"){
+			while(length){
+				*dest0 = color ^ *dest;
+				dest++;
+				dest0++;
+				length--;
+			}
+		}else static if(T.stringof == "ushort"){
+			if(length){
+				*dest0 = color ^ *dest;
+			}
+		}
+	}else{
+		while(length){
+			*dest0 = color ^ *dest;
+			dest++;
+			dest0++;
+			length--;
+		}
+	}
+}
+/**
+ * XOR blitter. Popularly used for selection and pseudo-transparency.
+ */
+public void xorBlitter(T)(T* dest, size_t length, T color) @nogc pure nothrow {
+	static if(T.stringof == "ubyte"){
+		__vector(ubyte[16]) colorV;
+		static enum MAINLOOP_LENGTH = 16;
+		static enum HALFLOAD_LENGTH = 8;
+		static enum QUTRLOAD_LENGTH = 4;
+	}else static if(T.stringof == "ushort"){
+		__vector(ushort[8]) colorV;
+		static enum MAINLOOP_LENGTH = 8;
+		static enum HALFLOAD_LENGTH = 4;
+		static enum QUTRLOAD_LENGTH = 2;
+	}else static if(T.stringof == "uint"){
+		__vector(uint[4]) colorV;
+		static enum MAINLOOP_LENGTH = 4;
+		static enum HALFLOAD_LENGTH = 2;
+		static enum QUTRLOAD_LENGTH = 1;
+	}else static assert(0, "Template parameter '"~ T.stringof ~"' not supported!");
+	for (int i ; i < colorV.length ; i++){
+		colorV[i] = color;
+	}
+	while(length >= MAINLOOP_LENGTH){
+		__m128i destV = _mm_loadu_si128(cast(__m128i*)dest);
+		destV = _mm_xor_si128(destV, colorV);
+		_mm_storeu_si128(cast(__m128i*)dest, destV);
+		dest += MAINLOOP_LENGTH;
+		length -= MAINLOOP_LENGTH;
+	}
+	if(length >= HALFLOAD_LENGTH){
+		__m128i destV = _mm_loadl_epi64(cast(__m64*)dest);
+		destV = _mm_xor_si128(destV, colorV);
+		_mm_storel_epi64(cast(__m64*)dest, destV);
+		dest += HALFLOAD_LENGTH;
+		length -= HALFLOAD_LENGTH;
+	}
+	if(length >= QUTRLOAD_LENGTH){
+		__m128i destV = _mm_cvtsi32_si128((*cast(int*)dest));
+		destV = _mm_xor_si128(destV, colorV);
+		*cast(int*)dest = _mm_cvtsi128_si32(destV);
+		static if(T.stringof != "uint"){
+			dest += QUTRLOAD_LENGTH;
+			length -= QUTRLOAD_LENGTH;
+		}
+	}
+	static if(T.stringof == "ubyte"){
+		while(length){
+			*dest = color ^ *dest;
+			dest++;
+			length--;
+		}
+	}else static if(T.stringof == "ushort"){
+		if(length){
+			*dest = color ^ *dest;
+		}
+	}
+	
+}
+/**
+ * XOR blitter. Popularly used for selection and pseudo-transparency.
+ */
+public void xorBlitter(T)(T* src, T* dest, size_t length) @nogc pure nothrow {
+	static if(T.stringof == "ubyte"){
+		static enum MAINLOOP_LENGTH = 16;
+		static enum HALFLOAD_LENGTH = 8;
+		static enum QUTRLOAD_LENGTH = 4;
+	}else static if(T.stringof == "ushort"){
+		static enum MAINLOOP_LENGTH = 8;
+		static enum HALFLOAD_LENGTH = 4;
+		static enum QUTRLOAD_LENGTH = 2;
+	}else static if(T.stringof == "uint"){
+		static enum MAINLOOP_LENGTH = 4;
+		static enum HALFLOAD_LENGTH = 2;
+		static enum QUTRLOAD_LENGTH = 1;
+	}else static assert(0, "Template parameter '"~ T.stringof ~"' not supported!");
+	while(length >= MAINLOOP_LENGTH){
+		__m128i srcV = _mm_loadu_si128(cast(__m128i*)src);
+		__m128i destV = _mm_loadu_si128(cast(__m128i*)dest);
+		destV = _mm_xor_si128(destV, srcV);
+		_mm_storeu_si128(cast(__m128i*)dest, destV);
+		dest += MAINLOOP_LENGTH;
+		src += MAINLOOP_LENGTH;
+		length -= MAINLOOP_LENGTH;
+	}
+	if(length >= HALFLOAD_LENGTH){
+		__m128i srcV = _mm_loadl_epi64(cast(__m64*)src);
+		__m128i destV = _mm_loadl_epi64(cast(__m64*)dest);
+		destV = _mm_xor_si128(destV, srcV);
+		_mm_storel_epi64(cast(__m64*)dest, destV);
+		dest += HALFLOAD_LENGTH;
+		src += HALFLOAD_LENGTH;
+		length -= HALFLOAD_LENGTH;
+	}
+	if(length >= QUTRLOAD_LENGTH){
+		__m128i srcV = _mm_cvtsi32_si128((*cast(int*)src));
+		__m128i destV = _mm_cvtsi32_si128((*cast(int*)dest));
+		destV = _mm_xor_si128(destV, srcV);
+		*cast(int*)dest = _mm_cvtsi128_si32(destV);
+		static if(T.stringof != "uint"){
+			dest += QUTRLOAD_LENGTH;
+			src += QUTRLOAD_LENGTH;
+			length -= QUTRLOAD_LENGTH;
+		}
+	}
+	static if(T.stringof == "ubyte"){
+		while(length){
+			*dest = *src ^ *dest;
+			dest++;
+			src++;
+			length--;
+		}
+	}else static if(T.stringof == "ushort"){
+		if(length){
+			*dest = *src ^ *dest;
+		}
+	}
+}
+/**
+ * XOR blitter. Popularly used for selection and pseudo-transparency.
+ */
+public void xorBlitter(T)(T* src, T* dest, T* dest0, size_t length) @nogc pure nothrow {
+	static if(T.stringof == "ubyte"){
+		static enum MAINLOOP_LENGTH = 16;
+		static enum HALFLOAD_LENGTH = 8;
+		static enum QUTRLOAD_LENGTH = 4;
+	}else static if(T.stringof == "ushort"){
+		static enum MAINLOOP_LENGTH = 8;
+		static enum HALFLOAD_LENGTH = 4;
+		static enum QUTRLOAD_LENGTH = 2;
+	}else static if(T.stringof == "uint"){
+		static enum MAINLOOP_LENGTH = 4;
+		static enum HALFLOAD_LENGTH = 2;
+		static enum QUTRLOAD_LENGTH = 1;
+	}else static assert(0, "Template parameter '"~ T.stringof ~"' not supported!");
+	while(length >= MAINLOOP_LENGTH){
+		__m128i srcV = _mm_loadu_si128(cast(__m128i*)src);
+		__m128i destV = _mm_loadu_si128(cast(__m128i*)dest);
+		destV = _mm_xor_si128(destV, srcV);
+		_mm_storeu_si128(cast(__m128i*)dest0, destV);
+		dest += MAINLOOP_LENGTH;
+		dest0 += MAINLOOP_LENGTH;
+		src += MAINLOOP_LENGTH;
+		length -= MAINLOOP_LENGTH;
+	}
+	if(length >= HALFLOAD_LENGTH){
+		__m128i srcV = _mm_loadl_epi64(cast(__m64*)src);
+		__m128i destV = _mm_loadl_epi64(cast(__m64*)dest);
+		destV = _mm_xor_si128(destV, srcV);
+		_mm_storel_epi64(cast(__m64*)dest0, destV);
+		dest += HALFLOAD_LENGTH;
+		dest0 += HALFLOAD_LENGTH;
+		src += HALFLOAD_LENGTH;
+		length -= HALFLOAD_LENGTH;
+	}
+	if(length >= QUTRLOAD_LENGTH){
+		__m128i srcV = _mm_cvtsi32_si128((*cast(int*)src));
+		__m128i destV = _mm_cvtsi32_si128((*cast(int*)dest));
+		destV = _mm_xor_si128(destV, srcV);
+		*cast(int*)dest0 = _mm_cvtsi128_si32(destV);
+		static if(T.stringof != "uint"){
+			dest += QUTRLOAD_LENGTH;
+			dest0 += QUTRLOAD_LENGTH;
+			src += QUTRLOAD_LENGTH;
+			length -= QUTRLOAD_LENGTH;
+		}
+	}
+	static if(T.stringof == "ubyte"){
+		while(length){
+			*dest = *src ^ *dest;
+			dest++;
+			dest0++;
+			src++;
+			length--;
+		}
+	}else static if(T.stringof == "ushort"){
+		if(length){
+			*dest0 = *src ^ *dest;
+		}
+	}
+	
+}
+unittest {
+    //test for zero correctness.
+    {
+        ubyte[255] a, b, c;
+        textBlitter(a.ptr, b.ptr, 255, 0);
+        testArrayForZeros(b);
+        textBlitter(a.ptr, b.ptr, c.ptr, 255, 0);
+        testArrayForZeros(c);
+        xorBlitter(a.ptr, b.ptr, 255);
+        testArrayForZeros(b);
+        xorBlitter(a.ptr, b.ptr, c.ptr, 255);
+        testArrayForZeros(c);
+        xorBlitter(b.ptr, 255, 0);
+        testArrayForZeros(b);
+        xorBlitter(b.ptr, c.ptr, 255, 0);
+        testArrayForZeros(c);
+    }
+    {
+        ushort[255] a, b, c;
+        textBlitter(a.ptr, b.ptr, 255, 0);
+        testArrayForZeros(b);
+        textBlitter(a.ptr, b.ptr, c.ptr, 255, 0);
+        testArrayForZeros(c);
+        xorBlitter(a.ptr, b.ptr, 255);
+        testArrayForZeros(b);
+        xorBlitter(a.ptr, b.ptr, c.ptr, 255);
+        testArrayForZeros(c);
+        xorBlitter(b.ptr, 255, 0);
+        testArrayForZeros(b);
+        xorBlitter(b.ptr, c.ptr, 255, 0);
+        testArrayForZeros(c);
+    }
+    {
+        uint[255] a, b, c;
+        textBlitter(a.ptr, b.ptr, 255, 0);
+        testArrayForZeros(b);
+        textBlitter(a.ptr, b.ptr, c.ptr, 255, 0);
+        testArrayForZeros(c);
+        xorBlitter(a.ptr, b.ptr, 255);
+        testArrayForZeros(b);
+        xorBlitter(a.ptr, b.ptr, c.ptr, 255);
+        testArrayForZeros(c);
+        xorBlitter(b.ptr, 255, 0);
+        testArrayForZeros(b);
+        xorBlitter(b.ptr, c.ptr, 255, 0);
+        testArrayForZeros(c);
+    }
+}
--- src/CPUblit/draw.d
+++ src/CPUblit/draw.d
@@ -5,21 +5,18 @@ import CPUblit.colorspaces;
  * Draws a line using a fixed point method. Is capable of drawing lines diagonally.
  */
 public void drawLine(T)(int x0, int y0, int x1, int y1, T color, T* dest, size_t destWidth) @nogc nothrow pure {
-	/+static if(!(T.stringof == "ubyte" || T.stringof == "ushort" || T.stringof == "uint" || T.stringof == "Color32Bit")){
-		static assert(0, "Template parameter '" ~ T.stringof ~ "' is not supported!");
-	}+/
 	if(x1 < x0){
-		int k = x1;
+		const int k = x1;
 		x1 = x0;
 		x0 = k;
 	}
 	if(y1 < y0){
-		int k = y1;
+		const int k = y1;
 		y1 = y0;
 		y0 = k;
 	}
-	int dx = x1 - x0;
-	int dy = y1 - y0;
+	const int dx = x1 - x0;
+	const int dy = y1 - y0;
 	if(!dx || !dy){
 		if(!dy){
 			sizediff_t offset = destWidth * y1;
@@ -36,24 +33,82 @@ public void drawLine(T)(int x0, int y0, int x1, int y1, T color, T* dest, size_t
 	}else if(dx>=dy){
 		int D = 2*dy - dx;
 		int y = y0;
-		for(int x = x0; x <= x1; x++){
+		for(int x = x0 ; x <= x1 ; x++){
 			dest[destWidth * y + x] = color;
 			if(D > 0){
 				y += 1;
-				D -= 2*dx;
+				D -= 2 * dx;
 			}
-			D += 2*dx;
+			D += 2 * dx;
 		}
 	}else{
-		int D = 2*dx - dy;
+		int D = 2 * dx - dy;
 		int x = x0;
-		for(int y = y0; y <= y1; y++){
+		for(int y = y0 ; y <= y1 ; y++){
 			dest[destWidth * y + x] = color;
 			if(D > 0){
 				x += 1;
-				D -= 2*dy;
+				D -= 2 * dy;
 			}
-			D += 2*dy;
+			D += 2 * dy;
+		}
+	}
+}
+/**
+ * Draws a line with the given pattern.
+ */
+public void drawLinePattern(T)(int x0, int y0, int x1, int y1, T[] pattern, T* dest, size_t destWidth) @nogc nothrow pure {
+	if(x1 < x0){
+		const int k = x1;
+		x1 = x0;
+		x0 = k;
+	}
+	if(y1 < y0){
+		const int k = y1;
+		y1 = y0;
+		y0 = k;
+	}
+	const int dx = x1 - x0;
+	const int dy = y1 - y0;
+	size_t patternPos;
+	if(!dx || !dy){
+		if(!dy){
+			sizediff_t offset = destWidth * y1;
+			for(int x = x0 ; x <= x1 ; x++){
+				dest[offset + x] = pattern[(patternPos++) % pattern.length];
+				
+			}
+		}else{
+			sizediff_t offset = destWidth * y0 + x0;
+			for(int y = y0 ; y <= y1 ; y++){
+				dest[offset] = pattern[(patternPos++) % pattern.length];
+				
+				offset += destWidth;
+			}
+		}
+	}else if(dx>=dy){
+		int D = 2 * dy - dx;
+		int y = y0;
+		for(int x = x0 ; x <= x1 ; x++){
+			dest[destWidth * y + x] = pattern[(patternPos++) % pattern.length];
+			//patternPos = patternPos + 1 < pattern.length ? patternPos + 1 : 0;
+			if(D > 0){
+				y += 1;
+				D -= 2 * dx;
+			}
+			D += 2 * dx;
+		}
+	}else{
+		int D = 2 * dx - dy;
+		int x = x0;
+		for(int y = y0 ; y <= y1 ; y++){
+			dest[destWidth * y + x] = pattern[(patternPos++) % pattern.length];
+			//patternPos = patternPos + 1 < pattern.length ? patternPos + 1 : 0;
+			if(D > 0){
+				x += 1;
+				D -= 2 * dy;
+			}
+			D += 2 * dy;
 		}
 	}
 }
@@ -71,27 +126,34 @@ public void drawRectangle(T)(int x0, int y0, int x1, int y1, T color, T*dest, si
 }
 /**
  * Draws a filled rectangle.
- * TODO: Upgrade algorhithm to use SSE2/MMX for faster filling.
  */
 public void drawFilledRectangle(T) (int x0, int y0, int x1, int y1, T color, T* dest, size_t destWidth) @nogc nothrow 
 		pure {
+	import core.stdc.string : memset;
+	import core.stdc.wchar_ : wmemset;
 	if(x1 < x0){
-		int k = x1;
+		const int k = x1;
 		x1 = x0;
 		x0 = k;
 	}
 	if(y1 < y0){
-		int k = y1;
+		const int k = y1;
 		y1 = y0;
 		y0 = k;
 	}
-	int targetWidth = y1 - y0;
-	
-	while(targetWidth){
-		drawLine!T(x0, y0, x1, y0, color, dest, destWidth);
-		y0++;
-		y1++;
-		targetWidth--;
+	int width = x1 - x0;
+	dest += x0;
+	for (int y = y0 ; y <= y1 ; y++) {
+		static if (is(T == ubyte)) {
+			memset(dest + (y * destWidth), color, width);
+		} else static if (is(T == ushort)) {
+			T* dest0 = dest + (y * destWidth);
+			for (int x ; x < width ; x++) {
+				dest0[x] = color;
+			}
+		} else static if (is(T == uint)) {
+			wmemset(dest + (y * destWidth), color, width);
+		}
 	}
 }
 /**
@@ -115,7 +177,8 @@ public void floodFill(T)(int x0, int y0, T color, T* dest, size_t destWidth, siz
 		}
 	}
 }
-@nogc nothrow pure unittest{
+unittest{
+	import std.conv : to;
 	{	//test if only the first line is being drawn.
 		ubyte[256*256] virtualImage;
 		drawLine(0, 0, 255, 0, 0xFF, virtualImage.ptr, 256);
@@ -136,8 +199,16 @@ public void floodFill(T)(int x0, int y0, T color, T* dest, size_t destWidth, siz
 			assert(virtualImage[(256 * y) + 1] == 0x00);
 		}
 	}
-	/+drawLine(0, 0, 0, 255, 255, virtualImage.ptr, 256);
-	drawLine(0, 0, 255, 255, 255, virtualImage.ptr, 256);
-	drawLine(0, 0, 255, 0, 255, virtualImage.ptr, 256);
-	floodFill(12, 12, 8, virtualImage.ptr, 256, 256*256);+/
+	{	//test if only the first line is being drawn.
+		ubyte[256*256] virtualImage;
+		ubyte[3] pattern = [0x20,0xFF,0x33];
+		drawLinePattern(0, 0, 255, 0, pattern, virtualImage.ptr, 256);
+		for(int x ; x < 256 ; x++){
+			assert(virtualImage[x] == pattern[x%3], "Error at position " ~ to!string(x));
+		}
+		for(int x ; x < 256 ; x++){
+			assert(virtualImage[256 + x] == 0x00);
+		}
+	}
+	
 }
