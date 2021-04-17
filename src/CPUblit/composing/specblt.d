5da3773dd3778df206925902752c8c76b475bb52
ziltoid1991 <laszloszeremi@outlook.com>
2021-04-18 01:39:40 +0200

    Added blitter with binary OR and AND

--- src/CPUblit/composing/specblt.d
+++ src/CPUblit/composing/specblt.d
@@ -450,53 +450,324 @@ public void xorBlitter(T)(T* src, T* dest, T* dest0, size_t length) @nogc pure n
 			*dest0 = *src ^ *dest;
 		}
 	}
-	
+}
+/**
+ * AND blitter for misc. usage.
+ */
+public void andBlitter(T)(T* src, T* dest, size_t length) {
+	static if(T.stringof == "ubyte"){
+		static enum MAINLOOP_LENGTH = 16;
+		static enum HALFLOAD_LENGTH = 8;
+		static enum QUTRLOAD_LENGTH = 4;
+	}else static if(T.stringof == "ushort"){
+		static enum MAINLOOP_LENGTH = 8;
+		static enum HALFLOAD_LENGTH = 4;
+		static enum QUTRLOAD_LENGTH = 2;
+	}else static if(T.stringof == "uint"){
+		static enum MAINLOOP_LENGTH = 4;
+		static enum HALFLOAD_LENGTH = 2;
+		static enum QUTRLOAD_LENGTH = 1;
+	}else static assert(0, "Template parameter '"~ T.stringof ~"' not supported!");
+	while(length >= MAINLOOP_LENGTH){
+		__m128i srcV = _mm_loadu_si128(cast(__m128i*)src);
+		__m128i destV = _mm_loadu_si128(cast(__m128i*)dest);
+		destV = _mm_and_si128(destV, cast(__m128i)srcV);
+		_mm_storeu_si128(cast(__m128i*)dest, destV);
+		dest += MAINLOOP_LENGTH;
+		src += MAINLOOP_LENGTH;
+		length -= MAINLOOP_LENGTH;
+	}
+	if(length >= HALFLOAD_LENGTH){
+		__m128i srcV = _mm_loadl_epi64(cast(__m128i*)src);
+		__m128i destV = _mm_loadl_epi64(cast(__m128i*)dest);
+		destV = _mm_and_si128(destV, srcV);
+		_mm_storel_epi64(cast(__m128i*)dest, destV);
+		dest += HALFLOAD_LENGTH;
+		src += HALFLOAD_LENGTH;
+		length -= HALFLOAD_LENGTH;
+	}
+	if(length >= QUTRLOAD_LENGTH){
+		__m128i srcV = _mm_cvtsi32_si128((*cast(int*)src));
+		__m128i destV = _mm_cvtsi32_si128((*cast(int*)dest));
+		destV = _mm_and_si128(destV, cast(__m128i)srcV);
+		_mm_storeu_si32(dest, destV);
+		static if(T.stringof != "uint"){
+			dest += QUTRLOAD_LENGTH;
+			src += QUTRLOAD_LENGTH;
+			length -= QUTRLOAD_LENGTH;
+		}
+	}
+	static if(T.stringof == "ubyte"){
+		while(length){
+			*dest = *src & *dest;
+			dest++;
+			src++;
+			length--;
+		}
+	}else static if(T.stringof == "ushort"){
+		if(length){
+			*dest = *src & *dest;
+		}
+	}
+}
+/**
+ * AND blitter for misc. usage.
+ */
+public void andBlitter(T)(T* src, T* dest, T* dest0, size_t length) {
+	static if(T.stringof == "ubyte"){
+		static enum MAINLOOP_LENGTH = 16;
+		static enum HALFLOAD_LENGTH = 8;
+		static enum QUTRLOAD_LENGTH = 4;
+	}else static if(T.stringof == "ushort"){
+		static enum MAINLOOP_LENGTH = 8;
+		static enum HALFLOAD_LENGTH = 4;
+		static enum QUTRLOAD_LENGTH = 2;
+	}else static if(T.stringof == "uint"){
+		static enum MAINLOOP_LENGTH = 4;
+		static enum HALFLOAD_LENGTH = 2;
+		static enum QUTRLOAD_LENGTH = 1;
+	}else static assert(0, "Template parameter '"~ T.stringof ~"' not supported!");
+	while(length >= MAINLOOP_LENGTH){
+		__m128i srcV = _mm_loadu_si128(cast(__m128i*)src);
+		__m128i destV = _mm_loadu_si128(cast(__m128i*)dest);
+		destV = _mm_and_si128(destV, cast(__m128i)srcV);
+		_mm_storeu_si128(cast(__m128i*)dest0, destV);
+		dest += MAINLOOP_LENGTH;
+		dest0 += MAINLOOP_LENGTH;
+		src += MAINLOOP_LENGTH;
+		length -= MAINLOOP_LENGTH;
+	}
+	if(length >= HALFLOAD_LENGTH){
+		__m128i srcV = _mm_loadl_epi64(cast(__m128i*)src);
+		__m128i destV = _mm_loadl_epi64(cast(__m128i*)dest);
+		destV = _mm_and_si128(destV, srcV);
+		_mm_storel_epi64(cast(__m128i*)dest0, destV);
+		dest += HALFLOAD_LENGTH;
+		dest0 += HALFLOAD_LENGTH;
+		src += HALFLOAD_LENGTH;
+		length -= HALFLOAD_LENGTH;
+	}
+	if(length >= QUTRLOAD_LENGTH){
+		__m128i srcV = _mm_cvtsi32_si128((*cast(int*)src));
+		__m128i destV = _mm_cvtsi32_si128((*cast(int*)dest));
+		destV = _mm_and_si128(destV, cast(__m128i)srcV);
+		_mm_storeu_si32(dest0, destV);
+		static if(T.stringof != "uint"){
+			dest += QUTRLOAD_LENGTH;
+			dest0 += QUTRLOAD_LENGTH;
+			src += QUTRLOAD_LENGTH;
+			length -= QUTRLOAD_LENGTH;
+		}
+	}
+	static if(T.stringof == "ubyte"){
+		while(length){
+			*dest0 = *src & *dest;
+			dest++;
+			dest0++;
+			src++;
+			length--;
+		}
+	}else static if(T.stringof == "ushort"){
+		if(length){
+			*dest0 = *src & *dest;
+		}
+	}
+}
+/**
+ * OR blitter for misc. usage.
+ */
+public void orBlitter(T)(T* src, T* dest, size_t length) {
+	static if(T.stringof == "ubyte"){
+		static enum MAINLOOP_LENGTH = 16;
+		static enum HALFLOAD_LENGTH = 8;
+		static enum QUTRLOAD_LENGTH = 4;
+	}else static if(T.stringof == "ushort"){
+		static enum MAINLOOP_LENGTH = 8;
+		static enum HALFLOAD_LENGTH = 4;
+		static enum QUTRLOAD_LENGTH = 2;
+	}else static if(T.stringof == "uint"){
+		static enum MAINLOOP_LENGTH = 4;
+		static enum HALFLOAD_LENGTH = 2;
+		static enum QUTRLOAD_LENGTH = 1;
+	}else static assert(0, "Template parameter '"~ T.stringof ~"' not supported!");
+	while(length >= MAINLOOP_LENGTH){
+		__m128i srcV = _mm_loadu_si128(cast(__m128i*)src);
+		__m128i destV = _mm_loadu_si128(cast(__m128i*)dest);
+		destV = _mm_or_si128(destV, cast(__m128i)srcV);
+		_mm_storeu_si128(cast(__m128i*)dest, destV);
+		dest += MAINLOOP_LENGTH;
+		src += MAINLOOP_LENGTH;
+		length -= MAINLOOP_LENGTH;
+	}
+	if(length >= HALFLOAD_LENGTH){
+		__m128i srcV = _mm_loadl_epi64(cast(__m128i*)src);
+		__m128i destV = _mm_loadl_epi64(cast(__m128i*)dest);
+		destV = _mm_or_si128(destV, srcV);
+		_mm_storel_epi64(cast(__m128i*)dest, destV);
+		dest += HALFLOAD_LENGTH;
+		src += HALFLOAD_LENGTH;
+		length -= HALFLOAD_LENGTH;
+	}
+	if(length >= QUTRLOAD_LENGTH){
+		__m128i srcV = _mm_cvtsi32_si128((*cast(int*)src));
+		__m128i destV = _mm_cvtsi32_si128((*cast(int*)dest));
+		destV = _mm_or_si128(destV, cast(__m128i)srcV);
+		_mm_storeu_si32(dest, destV);
+		static if(T.stringof != "uint"){
+			dest += QUTRLOAD_LENGTH;
+			src += QUTRLOAD_LENGTH;
+			length -= QUTRLOAD_LENGTH;
+		}
+	}
+	static if(T.stringof == "ubyte"){
+		while(length){
+			*dest = *src | *dest;
+			dest++;
+			src++;
+			length--;
+		}
+	}else static if(T.stringof == "ushort"){
+		if(length){
+			*dest = *src | *dest;
+		}
+	}
+}
+/**
+ * OR blitter for misc. usage.
+ */
+public void orBlitter(T)(T* src, T* dest, T* dest0, size_t length) {
+	static if(T.stringof == "ubyte"){
+		static enum MAINLOOP_LENGTH = 16;
+		static enum HALFLOAD_LENGTH = 8;
+		static enum QUTRLOAD_LENGTH = 4;
+	}else static if(T.stringof == "ushort"){
+		static enum MAINLOOP_LENGTH = 8;
+		static enum HALFLOAD_LENGTH = 4;
+		static enum QUTRLOAD_LENGTH = 2;
+	}else static if(T.stringof == "uint"){
+		static enum MAINLOOP_LENGTH = 4;
+		static enum HALFLOAD_LENGTH = 2;
+		static enum QUTRLOAD_LENGTH = 1;
+	}else static assert(0, "Template parameter '"~ T.stringof ~"' not supported!");
+	while(length >= MAINLOOP_LENGTH){
+		__m128i srcV = _mm_loadu_si128(cast(__m128i*)src);
+		__m128i destV = _mm_loadu_si128(cast(__m128i*)dest);
+		destV = _mm_or_si128(destV, cast(__m128i)srcV);
+		_mm_storeu_si128(cast(__m128i*)dest, destV);
+		dest += MAINLOOP_LENGTH;
+		src += MAINLOOP_LENGTH;
+		length -= MAINLOOP_LENGTH;
+	}
+	if(length >= HALFLOAD_LENGTH){
+		__m128i srcV = _mm_loadl_epi64(cast(__m128i*)src);
+		__m128i destV = _mm_loadl_epi64(cast(__m128i*)dest);
+		destV = _mm_or_si128(destV, srcV);
+		_mm_storel_epi64(cast(__m128i*)dest, destV);
+		dest += HALFLOAD_LENGTH;
+		src += HALFLOAD_LENGTH;
+		length -= HALFLOAD_LENGTH;
+	}
+	if(length >= QUTRLOAD_LENGTH){
+		__m128i srcV = _mm_cvtsi32_si128((*cast(int*)src));
+		__m128i destV = _mm_cvtsi32_si128((*cast(int*)dest));
+		destV = _mm_or_si128(destV, cast(__m128i)srcV);
+		_mm_storeu_si32(dest, destV);
+		static if(T.stringof != "uint"){
+			dest += QUTRLOAD_LENGTH;
+			dest0 += QUTRLOAD_LENGTH;
+			src += QUTRLOAD_LENGTH;
+			length -= QUTRLOAD_LENGTH;
+		}
+	}
+	static if(T.stringof == "ubyte"){
+		while(length){
+			*dest0 = *src | *dest;
+			dest++;
+			dest0++;
+			src++;
+			length--;
+		}
+	}else static if(T.stringof == "ushort"){
+		if(length){
+			*dest0 = *src | *dest;
+		}
+	}
 }
 unittest {
-    //test for zero correctness.
-    {
-        ubyte[255] a, b, c;
-        textBlitter(a.ptr, b.ptr, 255, 0);
-        testArrayForValue(b);
-        textBlitter(a.ptr, b.ptr, c.ptr, 255, 0);
-        testArrayForValue(c);
-        xorBlitter(a.ptr, b.ptr, 255);
-        testArrayForValue(b);
-        xorBlitter(a.ptr, b.ptr, c.ptr, 255);
-        testArrayForValue(c);
-        xorBlitter(b.ptr, 255, 0);
-        testArrayForValue(b);
-        xorBlitter(b.ptr, c.ptr, 255, 0);
-        testArrayForValue(c);
-    }
-    {
-        ushort[255] a, b, c;
-        textBlitter(a.ptr, b.ptr, 255, 0);
-        testArrayForValue(b);
-        textBlitter(a.ptr, b.ptr, c.ptr, 255, 0);
-        testArrayForValue(c);
-        xorBlitter(a.ptr, b.ptr, 255);
-        testArrayForValue(b);
-        xorBlitter(a.ptr, b.ptr, c.ptr, 255);
-        testArrayForValue(c);
-        xorBlitter(b.ptr, 255, 0);
-        testArrayForValue(b);
-        xorBlitter(b.ptr, c.ptr, 255, 0);
-        testArrayForValue(c);
-    }
-    {
-        uint[255] a, b, c;
-        textBlitter(a.ptr, b.ptr, 255, 0);
-        testArrayForValue(b);
-        textBlitter(a.ptr, b.ptr, c.ptr, 255, 0);
-        testArrayForValue(c);
-        xorBlitter(a.ptr, b.ptr, 255);
-        testArrayForValue(b);
-        xorBlitter(a.ptr, b.ptr, c.ptr, 255);
-        testArrayForValue(c);
-        xorBlitter(b.ptr, 255, 0);
-        testArrayForValue(b);
-        xorBlitter(b.ptr, c.ptr, 255, 0);
-        testArrayForValue(c);
-    }
+	//test for zero correctness.
+	{
+		ubyte[255] a, b, c;
+		textBlitter(a.ptr, b.ptr, 255, 0);
+		testArrayForValue(b);
+		textBlitter(a.ptr, b.ptr, c.ptr, 255, 0);
+		testArrayForValue(c);
+		xorBlitter(a.ptr, b.ptr, 255);
+		testArrayForValue(b);
+		xorBlitter(a.ptr, b.ptr, c.ptr, 255);
+		testArrayForValue(c);
+		xorBlitter(b.ptr, 255, 0);
+		testArrayForValue(b);
+		xorBlitter(b.ptr, c.ptr, 255, 0);
+		testArrayForValue(c);
+
+		andBlitter(a.ptr, b.ptr, 255);
+		testArrayForValue(b);
+		andBlitter(a.ptr, b.ptr, c.ptr, 255);
+		testArrayForValue(c);
+
+		orBlitter(a.ptr, b.ptr, 255);
+		testArrayForValue(b);
+		orBlitter(a.ptr, b.ptr, c.ptr, 255);
+		testArrayForValue(c);
+	}
+	{
+		ushort[255] a, b, c;
+		textBlitter(a.ptr, b.ptr, 255, 0);
+		testArrayForValue(b);
+		textBlitter(a.ptr, b.ptr, c.ptr, 255, 0);
+		testArrayForValue(c);
+		xorBlitter(a.ptr, b.ptr, 255);
+		testArrayForValue(b);
+		xorBlitter(a.ptr, b.ptr, c.ptr, 255);
+		testArrayForValue(c);
+		xorBlitter(b.ptr, 255, 0);
+		testArrayForValue(b);
+		xorBlitter(b.ptr, c.ptr, 255, 0);
+		testArrayForValue(c);
+
+		andBlitter(a.ptr, b.ptr, 255);
+		testArrayForValue(b);
+		andBlitter(a.ptr, b.ptr, c.ptr, 255);
+		testArrayForValue(c);
+
+		orBlitter(a.ptr, b.ptr, 255);
+		testArrayForValue(b);
+		orBlitter(a.ptr, b.ptr, c.ptr, 255);
+		testArrayForValue(c);
+	}
+	{
+		uint[255] a, b, c;
+		textBlitter(a.ptr, b.ptr, 255, 0);
+		testArrayForValue(b);
+		textBlitter(a.ptr, b.ptr, c.ptr, 255, 0);
+		testArrayForValue(c);
+		xorBlitter(a.ptr, b.ptr, 255);
+		testArrayForValue(b);
+		xorBlitter(a.ptr, b.ptr, c.ptr, 255);
+		testArrayForValue(c);
+		xorBlitter(b.ptr, 255, 0);
+		testArrayForValue(b);
+		xorBlitter(b.ptr, c.ptr, 255, 0);
+		testArrayForValue(c);
+
+		andBlitter(a.ptr, b.ptr, 255);
+		testArrayForValue(b);
+		andBlitter(a.ptr, b.ptr, c.ptr, 255);
+		testArrayForValue(c);
+
+		orBlitter(a.ptr, b.ptr, 255);
+		testArrayForValue(b);
+		orBlitter(a.ptr, b.ptr, c.ptr, 255);
+		testArrayForValue(c);
+	}
 }
