a33c7367cbec5e0d5a379aa00aaa5ded9c3b1468
ZILtoid1991 <laszloszeremi@outlook.com>
2021-11-01 16:25:33 +0100

    New scaler architecture

--- src/CPUblit/transform.d
+++ src/CPUblit/transform.d
@@ -3,13 +3,113 @@ module CPUblit.transform;
 import CPUblit.colorspaces;
 import bitleveld.datatypes;
 
+/+/** 
+ * Horizontal scaling using nearest integer algorithm for per-line operations. Mainly created to solve problems in
+ * PixelPerfectEngine with sprite scaling
+ * Params:
+ *   src = Source of the line to be transformed. Must be large enough to support the destination output with the given
+ *   transformation params.
+ *   dest = Destination of the transformed line.
+ *   length = Length of the output. Must be less or eaqual than the dest buffer. Use function "scaleNearestLength" to 
+ *   calculate the needed output length.
+ *   trfmParam = Transformation parameter.
+ *   offset = Offset in the source line. This allows the source line to be offset by the given amount of fractional 
+ *   pixels. Default value is zero.
+ */
+public void horizontalScaleNearest(T)(T* src, T* dest, sizediff_t length, int trfmParam, size_t offset = 0) 
+		@nogc pure nothrow {
+	if(trfmParam < 0){
+		offset = (length<<10) - 1024 - offset;
+	}
+	while (length > 0) {
+		*dest = src[offset>>>10];
+		offset += trfmParam;
+		length--;
+		dest++;
+	}
+}+/
+/** 
+ * Horizontal scaling using nearest integer algorithm for per-line operations. Intended to use with arrays that might
+ * contain elements less than 8 bit in length.
+ * Params:
+ *   src = Source of the line to be transformed. Must be large enough to support the destination output with the given
+ *   transformation params.
+ *   dest = Destination of the transformed line.
+ *   length = Length of the output. Must be less or eaqual than the dest buffer. Use function "scaleNearestLength" to 
+ *   calculate the needed output length.
+ *   trfmParam = Transformation parameter.
+ *   offset = Offset in the source line. This allows the source line to be offset by the given amount of fractional 
+ *   pixels. Default value is zero.
+ */
+public void horizontalScaleNearest(ArrayType)(ArrayType src, ArrayType dest, sizediff_t length, int trfmParam, 
+		size_t offset = 0) @nogc pure nothrow {
+	if(trfmParam < 0){
+		offset += (src.length<<10) - 1024 - offset;
+	}
+	for (int i ; i < length ; i++) {
+		dest[i] = src[offset>>>10];
+		offset += trfmParam;
+	}
+}
+/+/** 
+ * Horizontal scaling using nearest integer algorithm for per-line operations. Uses pointers for both the palette and
+ * source. Intended for 8 bit or larger data.
+ * Params:
+ *   src = Source of the line to be transformed. Must be large enough to support the destination output with the given
+ *   transformation params.
+ *   dest = Destination of the transformed line.
+ *   palette = Palette. Should have enough elements for every index, or ensure that source wouldn't point that far.
+ *   length = Length of the output. Must be less or eaqual than the dest buffer. Use function "scaleNearestLength" to 
+ *   calculate the needed output length.
+ *   trfmParam = Transformation parameter.
+ *   offset = Offset in the source line. This allows the source line to be offset by the given amount of fractional 
+ *   pixels. Default value is zero.
+ */
+public void horizontalScaleNearestAndCLU(T, U)(T* src, U* dest, U* palette, sizediff_t length, int trfmParam, 
+		size_t offset = 0) @nogc pure nothrow {
+	if(trfmParam < 0){
+		offset += (length<<10) - 1024 - offset;
+	}
+	while (length > 0) {
+		*dest = palette[src[offset>>>10]];
+		offset += trfmParam;
+		length--;
+		dest++;
+	}
+}+/
+/** 
+ * Horizontal scaling using nearest integer algorithm for per-line operations. Uses arrays for source (compatible with 
+ * data types smaller than 8 bit)
+ * Params:
+ *   src = Source of the line to be transformed. Must be large enough to support the destination output with the given
+ *   transformation params.
+ *   dest = Destination of the transformed line.
+ *   palette = Palette. Should have enough elements for every index, or ensure that source wouldn't point that far.
+ *   length = Length of the output. Must be less or eaqual than the dest buffer. Use function "scaleNearestLength" to 
+ *   calculate the needed output length.
+ *   trfmParam = Transformation parameter.
+ *   offset = Offset in the source line. This allows the source line to be offset by the given amount of fractional 
+ *   pixels. Default value is zero.
+ */
+public void horizontalScaleNearestAndCLU(ArrayType, U)(ArrayType src, U* dest, U* palette, sizediff_t length, 
+		int trfmParam, size_t offset = 0) @nogc pure nothrow {
+	if(trfmParam < 0){
+		offset += (src.length<<10) - 1024 - offset;
+	}
+	while (length > 0) {
+		*dest = palette[src[offset>>>10]];
+		offset += trfmParam;
+		length--;
+		dest++;
+	}
+}
 /**
- * Horizontal scaling using nearest integer algorithm for per-line operations.
+ * Horizontal scaling using nearest integer algorithm for per-line operations. (Old, might get deprecated later on)
  * Works with most datatypes. Use a separate one for 4 bit.
  * Lenght determines the source's length.
  * trfmParam describes how the transformation is done. 1024 results in the same exact line. Larger values cause shrinkage, smaller omes growth. Negative values cause reflections.
  */
-public void horizontalScaleNearest(T)(T* src, T* dest, sizediff_t length, int trfmParam) @nogc pure nothrow {
+public void _horizontalScaleNearest(T)(T* src, T* dest, sizediff_t length, int trfmParam) @nogc pure nothrow {
 	int trfmParamA = trfmParam;
 	sizediff_t offset;
 	length <<= 10;
@@ -25,12 +125,12 @@ public void horizontalScaleNearest(T)(T* src, T* dest, sizediff_t length, int tr
 	}
 }
 /**
- * Horizontal scaling and color lookup using nearest integer algorithm for per-line operations.
+ * Horizontal scaling and color lookup using nearest integer algorithm for per-line operations. (Old, might get deprecated later on)
  * Works with most datatypes. Use a separate one for 4 bit.
  * Lenght determines the source's length.
  * trfmParam describes how the transformation is done. 1024 results in the same exact line. Larger values cause shrinkage, smaller omes growth. Negative values cause reflections.
  */
-public void horizontalScaleNearestAndCLU(T, U)(T* src, U* dest, U* palette, sizediff_t length, const int trfmParam)
+public void _horizontalScaleNearestAndCLU(T, U)(T* src, U* dest, U* palette, sizediff_t length, const int trfmParam)
 		@nogc pure nothrow {
 	int trfmParamA = trfmParam;
 	sizediff_t offset;
@@ -47,12 +147,12 @@ public void horizontalScaleNearestAndCLU(T, U)(T* src, U* dest, U* palette, size
 	}
 }
 /**
- * Horizontal scaling using nearest integer algorithm for per-line operations.
+ * Horizontal scaling using nearest integer algorithm for per-line operations. (Old, might get deprecated later on)
  * Works with 4 bit datatypes.
  * Lenght determines the source's length.
  * trfmParam describes how the transformation is done. 1024 results in the same exact line. Larger values cause shrinkage, smaller omes growth. Negative values cause reflections.
  */
-public void horizontalScaleNearest4Bit(ubyte* src, ubyte* dest, sizediff_t length, sizediff_t offset, 
+public void _horizontalScaleNearest4Bit(ubyte* src, ubyte* dest, sizediff_t length, sizediff_t offset, 
 		const int trfmParam) @nogc pure nothrow {
 	int trfmParamA = trfmParam;
 	length <<= 10;
@@ -70,13 +170,13 @@ public void horizontalScaleNearest4Bit(ubyte* src, ubyte* dest, sizediff_t lengt
 	}
 }
 /**
- * Horizontal scaling using nearest integer algorithm for per-line operations.
+ * Horizontal scaling using nearest integer algorithm for per-line operations. (Old, might get deprecated later on)
  * Works with 16, 8, 4, and 2 bit datatypes.
  * Lenght determines the source's length.
  * trfmParam describes how the transformation is done. 1024 results in the same exact line. Larger values cause shrinkage, smaller omes growth. Negative values cause reflections.
  * `ArrayType` should be bitleveld's NibbleArray or QuadArray, but also works with regular D arrays.
  */
-public void horizontalScaleNearest(ArrayType)(ArrayType src, ArrayType dest, sizediff_t length, sizediff_t offset, 
+public void _horizontalScaleNearest(ArrayType)(ArrayType src, ArrayType dest, sizediff_t length, sizediff_t offset, 
 		const int trfmParam) @nogc pure nothrow {
 	int trfmParamA = trfmParam;
 	size_t destPtr;
@@ -94,12 +194,12 @@ public void horizontalScaleNearest(ArrayType)(ArrayType src, ArrayType dest, siz
 	}
 }
 /**
- * Horizontal scaling and color lookup using nearest integer algorithm for per-line operations.
+ * Horizontal scaling and color lookup using nearest integer algorithm for per-line operations. (Old, might get deprecated later on)
  * Works with 4 bit datatypes.
  * Lenght determines the source's length.
  * trfmParam describes how the transformation is done. 1024 results in the same exact line. Larger values cause shrinkage, smaller omes growth. Negative values cause reflections.
  */
-public void horizontalScaleNearest4BitAndCLU(U)(ubyte* src, U* dest, U* palette, sizediff_t length, sizediff_t offset, 
+public void _horizontalScaleNearest4BitAndCLU(U)(ubyte* src, U* dest, U* palette, sizediff_t length, sizediff_t offset, 
 		const int trfmParam) @nogc pure nothrow {
 	int trfmParamA = trfmParam;
 	length <<= 10;
@@ -117,13 +217,13 @@ public void horizontalScaleNearest4BitAndCLU(U)(ubyte* src, U* dest, U* palette,
 	}
 }
 /**
- * Horizontal scaling and color lookup using nearest integer algorithm for per-line operations.
+ * Horizontal scaling and color lookup using nearest integer algorithm for per-line operations. (Old, might get deprecated later on)
  * Works with 16, 8, 4, and 2 bit datatypes.
  * Lenght determines the source's length.
  * trfmParam describes how the transformation is done. 1024 results in the same exact line. Larger values cause shrinkage, smaller omes growth. Negative values cause reflections.
  * `ArrayType` should be bitleveld's NibbleArray or QuadArray, but also works with regular D arrays.
  */
-public void horizontalScaleNearestAndCLU(PaletteType, ArrayType)(ArrayType src, PaletteType* dest, PaletteType* palette, 
+public void _horizontalScaleNearestAndCLU(PaletteType, ArrayType)(ArrayType src, PaletteType* dest, PaletteType* palette, 
 		sizediff_t length, sizediff_t offset, const int trfmParam) @nogc pure nothrow {
 	int trfmParamA = trfmParam;
 	length <<= 10;
@@ -140,7 +240,7 @@ public void horizontalScaleNearestAndCLU(PaletteType, ArrayType)(ArrayType src, 
 	}
 }
 /**
- * Returns the needed length of dest for the given trfmParam if the "nearest integer algorithm" used.
+ * Returns the needed length of dest for the given trfmParam if the "nearest integer algorithm" used. (Old, might get deprecated later on)
  * Works with both horizontal and vertical algorithms.
  */
 public size_t scaleNearestLength(size_t origLen, int trfmParam) @nogc @safe pure nothrow {
@@ -152,14 +252,28 @@ pure nothrow unittest{
 	import std.conv : to;
 	uint[256] a, b;
 	ubyte[256] c;
-	NibbleArray d = NibbleArray(c[0..$], 512);
-	QuadArray e = QuadArray(c[0..$], 1024);
-	//force the compiler to check the scalers
-	horizontalScaleNearest(a.ptr, b.ptr, 16, 2048);
-	horizontalScaleNearestAndCLU(c.ptr,a.ptr,b.ptr,16,2048);
-	horizontalScaleNearest4BitAndCLU(c.ptr,a.ptr,b.ptr,16,0,2048);
-	horizontalScaleNearestAndCLU(d, a.ptr, b.ptr, 256, 0, 2048);
-	horizontalScaleNearestAndCLU(e, a.ptr, b.ptr, 256, 0, 2048);
+	NibbleArray d = NibbleArray(c[0..$], 512), f = NibbleArray(c[0..$], 512);
+	QuadArray e = QuadArray(c[0..$], 1024), g = QuadArray(c[0..$], 1024);
+	//old scalers
+	_horizontalScaleNearest(a.ptr, b.ptr, 16, 2048);
+	_horizontalScaleNearestAndCLU(c.ptr,a.ptr,b.ptr,16,2048);
+	_horizontalScaleNearest4BitAndCLU(c.ptr,a.ptr,b.ptr,16,0,2048);
+	_horizontalScaleNearestAndCLU(d, a.ptr, b.ptr, 256, 0, 2048);
+	_horizontalScaleNearestAndCLU(e, a.ptr, b.ptr, 256, 0, 2048);
+	
+	horizontalScaleNearest(a, b, scaleNearestLength(100, 1000), 1000, 20);
+	horizontalScaleNearest(a, b, scaleNearestLength(100, -1000), -1000, 20);
+	horizontalScaleNearest(d, f, scaleNearestLength(200, 1000), 1000, 56);
+	horizontalScaleNearest(d, f, scaleNearestLength(200, -1000), -1000, 58);
+	horizontalScaleNearest(e, g, scaleNearestLength(200, 1100), 1100, 53);
+	horizontalScaleNearest(e, g, scaleNearestLength(200, -1100), -1100, 53);
+
+	horizontalScaleNearestAndCLU(c, a.ptr, b.ptr, scaleNearestLength(130, 1200), 1200, 94);
+	horizontalScaleNearestAndCLU(c, a.ptr, b.ptr, scaleNearestLength(130, -1200), -1200, 0);
+	horizontalScaleNearestAndCLU(d, a.ptr, b.ptr, scaleNearestLength(130, 1200), 1200, 94);
+	horizontalScaleNearestAndCLU(d, a.ptr, b.ptr, scaleNearestLength(130, -1200), -1200, 94);
+	horizontalScaleNearestAndCLU(e, a.ptr, b.ptr, scaleNearestLength(130, 1200), 1200, 94);
+	horizontalScaleNearestAndCLU(e, a.ptr, b.ptr, scaleNearestLength(130, -1200), -1200, 94);
 	assert(20 == scaleNearestLength(10, 512), "Error while testing function `scaleNearestLength`. Expected value: 20 " ~
 			"Returned value: " ~ to!string(scaleNearestLength(10, 512)));
 	assert(20 == scaleNearestLength(10, -512), "Error while testing function `scaleNearestLength`. Expected value: 20 " ~
